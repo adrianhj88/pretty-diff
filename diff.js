b2d73c3a1ac593a214cb7049227b1b42253d4fc8
Adrian Jørgensen <adrianhj@usit.uio.no>
2018-02-13 20:08:51 +0100

    Add mail and title placeholders

--- diff.js
+++ diff.js
@@ -1,66 +1,66 @@
-var spawn = require( "child_process" ).spawn;
+var spawn = require("child_process").spawn;
 
-module.exports = function( args, fn ) {
+module.exports = function(args, fn) {
 	var stdout = "";
 	var stderr = "";
 
-	var childArgs = [ "diff", "--no-color" ];
-	if ( args ) {
-		childArgs = childArgs.concat( args.split( /\s/ ) );
+	var childArgs = ["diff", "--no-color"];
+	if (args) {
+		childArgs = childArgs.concat(args.split(/\s/));
 	}
 
-	var child = spawn( "git", childArgs );
+	var child = spawn("git", childArgs);
 
-	child.stdout.on( "data", function( chunk ) {
+	child.stdout.on("data", function(chunk) {
 		stdout += chunk;
 	});
 
-	child.stderr.on( "data", function( chunk ) {
+	child.stderr.on("data", function(chunk) {
 		stderr += chunk;
 	});
 
-	child.on( "close", function( code ) {
+	child.on("close", function(code) {
 
 		// `git diff` will render a diff when comparing files that aren't part of a
 		// git repository, but it will exit with code 1 and nothing written to
 		// stderr. We detect this case and pretend that the exit code was 0 so that
 		// the diff gets displayed.
-		if ( code === 1 && !stderr && stdout ) {
+		if (code === 1 && !stderr && stdout) {
 			code = 0
 		}
 
-		if ( code !== 0 ) {
-			var error = new Error( stderr );
+		if (code !== 0) {
+			var error = new Error(stderr);
 			error.code = code;
-			fn( error );
-		} else if ( !stdout.length ) {
-			fn( null, null );
+			fn(error);
+		} else if (!stdout.length) {
+			fn(null, null);
 		} else {
-			fn( null, splitByFile( stdout ) );
+			fn(null, splitByFile(stdout));
 		}
 	});
 };
 
-function splitByFile( diff ) {
+function splitByFile(diff) {
 	var filename;
 	var isEmpty = true;
 	var files = {};
 
-	diff.split( "\n" ).forEach(function( line, i ) {
+	diff.split("\n").forEach(function(line, i) {
 
 		// Unmerged paths, and possibly other non-diffable files
 		// https://github.com/scottgonzalez/pretty-diff/issues/11
-		if ( !line || line.charAt( 0 ) === "*" ) {
+		if (!line || line.charAt(0) === "*") {
 			return;
 		}
 
-		if ( line.charAt( 0 ) === "d" ) {
+		if (line.charAt(0) === "d") {
 			isEmpty = false;
-			filename = line.replace( /^diff --(?:cc |git a\/)(\S+).*$/, "$1" );
-			files[ filename ] = [];
+			filename = line.replace(/^diff --(?:cc |git a\/)(\S+).*$/, "$1");
+			files[filename] = [];
 		}
 
-		files[ filename ].push( line );
+		files[filename].push(line);
 	});
 
 	return isEmpty ? null : files;
--- pretty-diff.js
+++ pretty-diff.js
@@ -1,69 +1,73 @@
 #!/usr/bin/env node
 
-var fs = require( "fs" );
-var path = require( "path" );
-var open = require( "open" );
-var diff = require( "./diff" );
+var fs = require("fs");
+var path = require("path");
+var open = require("open");
+var diff = require("./diff");
 
-diff( process.argv.slice( 2 ).join( " " ), function( error, parsedDiff ) {
-	if ( error ) {
+diff(process.argv.slice(2).join(" "), function(error, parsedDiff) {
+    if (error) {
+        // Usage error, assume we're not in a git directory
+        if (error.code === 129) {
+            process.stderr.write("Error: Not a git repository\n");
+            return;
+        }
 
-		// Usage error, assume we're not in a git directory
-		if ( error.code === 129 ) {
-			process.stderr.write( "Error: Not a git repository\n" );
-			return;
-		}
+        process.stderr.write(error.message);
+        return;
+    }
 
-		process.stderr.write( error.message );
-		return;
-	}
+    if (!parsedDiff) {
+        console.log("No differences");
+        return;
+    }
 
-	if ( !parsedDiff ) {
-		console.log( "No differences" );
-		return;
-	}
+    var mail = "mail@placeholder.com";
+    var title = "Title placeholder";
 
-	generatePrettyDiff( parsedDiff );
+    generatePrettyDiff(mail, title, parsedDiff);
 });
 
-function generatePrettyDiff( parsedDiff ) {
-	var template = fs.readFileSync( __dirname + "/template.html", "utf8" );
-	var diffHtml = "";
+function generatePrettyDiff(mail, title, parsedDiff) {
+    var template = fs.readFileSync(__dirname + "/template.html", "utf8");
+    var diffHtml = "";
     var path = "/tmp/mail.html";
 
-	for ( var file in parsedDiff ) {
-		diffHtml += "<h2>" + file + "</h2>" +
-		"<div class='file-diff'><div>" +
-			markUpDiff( parsedDiff[ file ] ) +
-		"</div></div>";
-	}
+    for (var file in parsedDiff) {
+        diffHtml += "<h2>" + file + "</h2>" +
+            "<div class='file-diff'><div>" + markUpDiff(parsedDiff[file]) + "</div></div>";
+    }
 
-	fs.writeFileSync( path, template.replace( "{{diff}}", diffHtml ), "utf8" );
+    template = template.replace(/{{mail}}/g, mail);
+    template = template.replace(/{{title}}/g, title);
+    template = template.replace(/{{diff}}/, diffHtml);
+
+    fs.writeFileSync(path, template, "utf8");
 }
 
 var markUpDiff = function() {
-	var diffClasses = {
-		"d": "file",
-		"i": "file",
-		"@": "info",
-		"-": "delete",
-		"+": "insert",
-		" ": "context"
-	};
+    var diffClasses = {
+        "d": "file",
+        "i": "file",
+        "@": "info",
+        "-": "delete",
+        "+": "insert",
+        " ": "context"
+    };
 
-	function escape( str ) {
-		return str
-			.replace( /\$/g, "$$$$" )
-			.replace( /&/g, "&amp;" )
-			.replace( /</g, "&lt;" )
-			.replace( />/g, "&gt;" )
-			.replace( /\t/g, "    " );
-	}
+    function escape(str) {
+        return str
+            .replace(/\$/g, "$$$$")
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/\t/g, "    ");
+    }
 
-	return function( diff ) {
-		return diff.map(function( line ) {
-			var type = line.charAt( 0 );
-			return "<pre class='" + diffClasses[ type ] + "'>" + escape( line ) + "</pre>";
-		}).join( "\n" );
-	};
+    return function(diff) {
+        return diff.map(function(line) {
+            var type = line.charAt(0);
+            return "<pre class='" + diffClasses[type] + "'>" + escape(line) + "</pre>";
+        }).join("\n");
+    };
 }();
